f37e518b3ecb2ac417489258c1c54527c3a65ae4
Cloud_kudryashov <sk-1999-73@mail.ru>
2021-02-24 20:55:23 +0300

    add function find_rang

--- Chain_pr/Chain_main.cpp
+++ Chain_pr/Chain_main.cpp
@@ -1,4 +1,139 @@
+#include <iostream>
+
+using namespace std;
+struct Rate_Catalog
+{
+	int pos_from; //[0,99]
+	double move_left_barieer;   
+	double move_right_barieer;   
+	double rate_left;
+	double rate_right;
+};
+
+Rate_Catalog * mas_rate = new Rate_Catalog[100];
+int mem_pos_from = 0;
+void find_rang(bool * chain_bool) {
+	int l = 1;
+	while (chain_bool[((int)100 + (mem_pos_from - l)) % 100] != true && l < 5) {// ищем разность позициий данного атома и ближайшего слева
+		l++;
+	}
+	int r = 1;
+	while (chain_bool[(mem_pos_from + r) % 100] != true && r < 5) {// ищем разность позициий данного атома и ближайшего справа
+		r++;
+	}
+	if (l == 1) { 
+		mas_rate->move_left_barieer = -1; 
+		if (r == 1) mas_rate->move_right_barieer = -1;
+		if (r == 2) mas_rate->move_right_barieer = 0.37;
+		if (r >= 3) mas_rate->move_right_barieer = 0.295;
+	}
+	else {
+		if (l == 2) {
+			switch (r)
+			{
+			case 1: 
+				mas_rate->move_left_barieer = 0.37;
+				mas_rate->move_right_barieer = -1;
+				break;
+			case 2:
+				mas_rate->move_left_barieer = 0.235;
+				mas_rate->move_right_barieer = 0.235;
+				break;
+			case 3:
+				mas_rate->move_left_barieer = 0.235;
+				mas_rate->move_right_barieer = 0.241;
+				break;
+			default:
+				mas_rate->move_left_barieer = 0.235;
+				mas_rate->move_right_barieer = 0.241;
+				break;
+			}
+		}
+		else { 
+			if (l == 3) {
+				switch (r)
+				{
+				case 1:
+					mas_rate->move_left_barieer = 0.295;
+					mas_rate->move_right_barieer = -1;
+					break;
+				case 2:
+					mas_rate->move_left_barieer = 0.241;
+					mas_rate->move_right_barieer = 0.235;
+					break;
+				default:
+					mas_rate->move_left_barieer = 0.241;
+					mas_rate->move_right_barieer = 0.241;
+					break;
+				}
+			}
+			else {
+				if (l >= 3) {
+					switch (r)
+					{
+					case 1:
+						mas_rate->move_left_barieer = 0.295;
+						mas_rate->move_right_barieer = -1;
+						break;
+					case 2:
+						mas_rate->move_left_barieer = 0.241;
+						mas_rate->move_right_barieer = 0.235;
+						break;
+					default:
+						mas_rate->move_left_barieer = 0.241;
+						mas_rate->move_right_barieer = 0.241;
+						break;
+					}
+				}
+			}
+			
+		}
+	}
+	
+};
+void find_rate() {
+
+};
+void create_rate_catalog() {
+	for (int i = mem_pos_from - 3; i < mem_pos_from + 3; i++) {
+		int rang[3] = { -1,0,-1 };
+		
+	}
+};
+void choose_event() {
+	//
+
+	//
+	mem_pos_from = -1;
+}
+void move_atom() {
+
+}
+
 int main()
 {
+	int * chain_int = new int[100];
+	bool * chain_bool = new bool[100];
+
+	chain_bool[97] = true;
+	chain_bool[2] = true;
+	mem_pos_from = 0;
+	
+	find_rang(chain_bool);
+	cout << mas_rate->move_left_barieer << endl;
+	cout << mas_rate->move_right_barieer << endl;
+
+
+	//1 ЭТАП (Напыление)
+	//create_rate_catalog();
+	//choose_event();
+	//move_atom();
+
+	
+
+	delete[] chain_bool;
+	delete[] chain_int;
+	delete[] mas_rate;
+	getchar();
 	return 0;
 }
